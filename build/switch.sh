59d5e70d87a014daf760ede99dc765154c0bfeb6
Matus Mastena <matus@MatuusPC.matuuspc.tenmatus.sk>
2023-02-09 15:01:52 +0100

    modified:   build.conf 	modified:   build/switch.sh 	modified:   grub.cfg 	modified:   minimal-option

--- build/switch.sh
+++ build/switch.sh
@@ -15,18 +15,10 @@ curl -1sLf 'https://dl.cloudsmith.io/public/matus-mastena-Hcr/matuusos/config.de
 # Create distribution configuration files
 mkdir -p /etc/skel/.config
 git clone https://github.com/MatuusOS/i3-dotfiles
-# Install rhino-deinst onto system
-mkdir ~/creation/rhino-deinst
-cd ~/creation/rhino-deinst || exit
-wget -q https://github.com/rollingrhinoremix/rhino-deinst/releases/latest/download/rhino-deinst
-chmod +x ~/creation/rhino-deinst/rhino-deinst
-mv ~/creation/rhino-deinst/rhino-deinst /usr/bin
-# Install the latest Linux kernel (from Ubuntu mainline repositories)
-mkdir ~/creation/kernel
-cd ~/creation/kernel || exit
+# Setup Xanmod
 echo 'deb http://deb.xanmod.org releases main' | sudo tee ~/creation/etc/apt/sources.list.d/xanmod-kernel.list
 sudo apt update
-sudo apt install ./*.deb -y
+sudo apt install linux-xanmod-x64v3 -y
 # Install Nala
 apt-get install nala -y
 # Clean up system files
@@ -35,13 +27,12 @@ sed -i 's/kinetic/devel/g' /etc/apt/sources.list
 sed -i 's/kinetic/devel/g' /etc/lsb-release
 sed -i 's/kinetic/devel/g' /usr/lib/os-release
 #So much release info that are mostly same!
-sed -i 's/PRETTY_NAME="Ubuntu Kinetic Kudu (development branch)"/PRETTY_NAME="MatuusOS Bratislava"/g' /etc/os-release
+sed -i 's/PRETTY_NAME="Ubuntu Kinetic Kudu (development branch)"/PRETTY_NAME="MatuusOS Flowing"/g' /etc/os-release
 sed -i 's%HOME_URL="https://www.ubuntu.com/"%HOME_URL="https://www.matuusos.github.io"%g' /etc/os-release
 apt-get --allow-releaseinfo-change update -y
 apt-get --allow-releaseinfo-change dist-upgrade -y
 apt-get autopurge -y
 apt-get clean
-
 sed -i 's/^set -e//g' /var/lib/dpkg/info/snapd.prerm #For minimal as snapd fails at some point
 sed -i 's/^set -e//g' /var/lib/dpkg/info/snapd.postrm
 echo 'find / -type f -name "*snap*" -delete 2> /dev/null' >> /var/lib/dpkg/info/snapd.postrm #to make snap is fully removed
